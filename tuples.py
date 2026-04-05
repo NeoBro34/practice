704c3237e23ff7c273fce5e43c21faaa73b30d6f
Nurislom ( N E O ) <nurislomkhurramov@gmail.com>
2026-04-06 00:46:08 +0900

    feat: learn tuple and list

---
+++ tuples.py
@@ -0,0 +1,26 @@
+'''Tuple
+    (1) What is tuple: typle vs list
+    (2) Unpacking arguments
+    (3) zip
+'''
+print("===== What is tuple: typle vs list =====")
+# Java/PHP/NodeJS array => Python list
+
+# Literal
+numbs = [3, 5, 1, 2]
+
+# constructor
+letters = list("Hello World!")
+
+fruits = ["apple", "Lemon", "banana", "kiwi"]
+print("before fruits:", fruits)
+
+fruits[2] = "melon"
+print("after fruits:", fruits)
+
+# we can not mutate tuple
+animals = ("dog", "cat", "fish", "lion")
+tuple_obj = ("MIT", 100, True, None)
+
+print(animals[0])
+# animals [0] = "bird"
