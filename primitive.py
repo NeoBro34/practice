b2fff3eff47965058720b739c5c97a70ca3d41c6
Nurislom ( N E O ) <nurislomkhurramov@gmail.com>
2026-04-05 17:17:47 +0900

    feat: learn numbers in python

---
+++ primitive.py
@@ -0,0 +1,14 @@
+print("===== number =====")
+# in JAVA, variable is a name storage location!
+# in Python, variable is named referance!
+
+count = 100
+count_type = type(count)
+# buyerda (f) nodeJsdagi (``) super string bilan bir xil vazifada
+print(f"the count: {count} and type: {count_type}")
+
+# (Pythonda xamma narsa objectdir xox u number bo'lsin xox u string bo'lsin!) !!! STATE hamda METHODlariga egadir!
+
+result1 = count.bit_count()  # method
+result2 = count.numerator  # state
+print(result1, result2)
